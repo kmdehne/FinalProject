4fa36c3e34cef5239a9fe9c34cc749b0e0d7ca3e
Kory Dehne <kmdehne@pluto.dsu.edu>
2018-05-05 14:52:34 -0500

    FindDirectory

--- FinalProject3/Program.cs
+++ FinalProject3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,30 @@ namespace FinalProject3
     {
         static void Main(string[] args)
         {
+            string directory;
+            Console.WriteLine("Please enter desired directory or type 'dir' to use current directory");
+            string filepath = Console.ReadLine();
+            if (filepath != null)
+            {
+                if (filepath == "dir")
+                {
+                    directory = Directory.GetCurrentDirectory();
+                    Directory.SetCurrentDirectory(directory);
+                    Console.WriteLine("Current directory is: " + Directory.GetCurrentDirectory());
+                }
+                else if (Directory.Exists(filepath))
+                {
+                    directory = filepath;
+                    Directory.SetCurrentDirectory(directory);
+                    Console.WriteLine("Current directory is: " + Directory.GetCurrentDirectory());
+                }
+                else
+                {
+                    Console.WriteLine("Directory not found");
+                }
+            }
+            Console.WriteLine("Press any key to continue...");
+            var duplicates{ }
         }
     }
 }
