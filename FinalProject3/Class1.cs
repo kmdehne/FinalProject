093f1c692b1cd57109b583fbe03f865ec1a3951f
Kory Dehne <kmdehne@pluto.dsu.edu>
2018-05-05 18:50:22 -0500

    FindingDuplicates

---
+++ FinalProject3/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject3
+{
+    public class Duplicate
+    {
+        public string FileName { get; set; }
+        public string FileHash { get; set; }
+                
+    }
+}
--- FinalProject3/Program.cs
+++ FinalProject3/Program.cs
@@ -2,30 +2,32 @@
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FinalProject3
 {
-    class Program
+    public class Program
     {
+        static public string dir;
         static void Main(string[] args)
         {
-            string directory;
+            //Enter directory or use current directory
             Console.WriteLine("Please enter desired directory or type 'dir' to use current directory");
             string filepath = Console.ReadLine();
             if (filepath != null)
             {
                 if (filepath == "dir")
                 {
-                    directory = Directory.GetCurrentDirectory();
-                    Directory.SetCurrentDirectory(directory);
+                    dir = Directory.GetCurrentDirectory();
+                    Directory.SetCurrentDirectory(dir);
                     Console.WriteLine("Current directory is: " + Directory.GetCurrentDirectory());
                 }
                 else if (Directory.Exists(filepath))
                 {
-                    directory = filepath;
-                    Directory.SetCurrentDirectory(directory);
+                    dir = filepath;
+                    Directory.SetCurrentDirectory(dir);
                     Console.WriteLine("Current directory is: " + Directory.GetCurrentDirectory());
                 }
                 else
@@ -33,8 +35,36 @@ namespace FinalProject3
                     Console.WriteLine("Directory not found");
                 }
             }
-            Console.WriteLine("Press any key to continue...");
-            var duplicates{ }
+            Console.WriteLine("Counting Files...");
+            System.IO.DirectoryInfo directory = new System.IO.DirectoryInfo(dir);
+            var filecount = Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories);
+            var filelist = Directory.GetFiles(dir);
+            Console.WriteLine(filecount.Length + " files found");
+
+            //Finding duplicate files
+            List<Duplicate> details = new List<Duplicate>();
+            List<string> delete = new List<string>();
+            foreach(var item in filelist)
+            {
+                using (var fs = new FileStream(item, FileMode.Open, FileAccess.Read))
+                {
+                    details.Add(new Duplicate()
+                    {
+                        FileName = item,
+                        FileHash = BitConverter.ToString(SHA1.Create().ComputeHash(fs)),
+                    });
+                }
+            }
+
+
+            var DuplicateList = details.GroupBy(f => f.FileHash).Select(g =>
+            new { FileHash = g.Key, Files = g.Select(z => z.FileName).ToList() });
+
+
+
+            delete.AddRange(DuplicateList.SelectMany(f => f.Files.Skip(1)).ToList());
+            Console.WriteLine("Total files - {0}", delete.Count);
+
         }
     }
 }
